4433563a23f29bc83942b3755260dfb1721850f3
Shakib29 <141607998+Shakib29@users.noreply.github.com>
2025-09-14 17:32:23 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
